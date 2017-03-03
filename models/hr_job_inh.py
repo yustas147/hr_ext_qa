e9f62cc455eec8e46ec53c4b358f96b4aafa8abf
yustas147 <yustas@simbioz.com.ua>
2017-03-03 10:57:02 +0200

    next part of fields added

--- models/hr_job_inh.py
+++ models/hr_job_inh.py
@@ -8,7 +8,12 @@ class hr_job(models.Model):
     
     start_date = fields.Date(string="Start date")
     time_to_expire = fields.Date(string="Time to expire")
-    time_to_expire = fields.Selection(string="Priority", selection=[('normal','Normal'),('high','High'),('very_high','Very High')])    
+    priority = fields.Selection(string="Priority", selection=[('normal','Normal'),('high','High'),('very_high','Very High')])    
     step_technologies = fields.Text(string="Step Technologies")
+    duties = fields.Text(string="Duties")
+    requirement = fields.Text(string="Requirement")
     phase_proj_prod = fields.Selection(string="Phase project/product", selection=[('new','New'),('existing','Existing'),('other','Other')])    
     team_size = fields.Integer(string="Team size")
+    remote_allowed = fields.Selection(string="Remote Allowed?", selection=[('allowed','Allowed'),('disallowed','Disallowed'),('partially_allowed','Partiallly Allowed')])    
+    project_description = fields.Text(string="Project Description")    
+    application_ids = fields.Many2many(string="Applications", comodel_name="hr.applicant", relation="hr_job_applicant")
