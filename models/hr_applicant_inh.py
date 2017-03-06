60cfbe41db54d09378fd209a329cc418fb691ebb
yustas147 <yustas@simbioz.com.ua>
2017-03-06 18:55:15 +0200

    skype

--- models/hr_applicant_inh.py
+++ models/hr_applicant_inh.py
@@ -13,46 +13,7 @@ class hr_applicant(models.Model):
     candidate_type = fields.Selection(string="Candidate Type", selection=[('normal','Normal'),('high','High'),('very_high','Very High')])
     current_salary = fields.Float(string="Current Salary")
     availability_date = fields.Date(string="Available From")
+    #skype = fields.Char(string="Skype")
+    skype = fields.Char(string="Skype", related='partner_id.skype')
     
-    #name = fields.Char('Name')
-    #percentage = fields.Integer(string='Grade of owning')
-    #description = fields.Text(string='Full skill owning employee description')    
-    #employee_id = fields.Many2one(comodel_name='hr.employee', string=None)
-    #skill_id = fields.Many2one(comodel_name='hr.skill', string=None)
-    
-    #@api.model
-    #def sync_skills(self,obj):
-        #_logger.info('self is %s' % (unicode(self)))
-        #emp = obj.employee_id
-#        emp.update({"skill_ids":[(4,obj.skill_id.id)]})
-        #sk_ids = emp.hr_employee_skill_data_ids.mapped('skill_id').mapped('id')
-        #_logger.info('sk_ids is %s' % (sk_ids)) 
-        #emp.update({"skill_ids":[(6,0,sk_ids)]})
-        
-        
-#class hr_skill(models.Model):
-    #_name = 'hr.skill'    
-    #_inherit = 'hr.skill'    
-    
-    #hr_employee_skill_data_ids = fields.One2many(comodel_name='hr.employee.skill.data', 
-                                                #inverse_name='skill_id', 
-                                                #string='Employee data')                                                
-    
-    
-#class hr_employee(models.Model):
-    #_name = 'hr.employee'    
-    #_inherit = 'hr.employee'    
-    
-    #hr_employee_skill_data_ids = fields.One2many(comodel_name='hr.employee.skill.data', 
-                                                #inverse_name='employee_id', 
-                                                #string='Skills')                                                
-                                                
-    #@api.model
-    #def sync_skills_2(self,obj):
-        #_logger.info('self is %s' % (unicode(self)))
-        #emp = obj
-#        emp.update({"skill_ids":[(4,obj.skill_id.id)]})
-        #sk_ids = emp.hr_employee_skill_data_ids.mapped('skill_id').mapped('id')
-        #_logger.info('sk_ids is %s' % (sk_ids)) 
-        #emp.update({"skill_ids":[(6,0,sk_ids)]})
     
