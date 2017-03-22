b941bce875a110907e66f68ffe9c2102a1fdbd3b
yustas147 <yustas@simbioz.com.ua>
2017-03-22 16:34:25 +0200

    face fix 6

--- models/hr_job_inh.py
+++ models/hr_job_inh.py
@@ -4,16 +4,24 @@ from openerp import models, fields, api
 
 class hr_job(models.Model):
     _name = 'hr.job'    
-    _inherit = 'hr.job'    
+    _inherit = 'hr.job'   
     
-    start_date = fields.Date(string="Start date", required=True)
+    @api.model
+    def _set_sd(self):    
+        return fields.Date.context_today(self)    
+    
+    #start_date = fields.Date(string="Start date", default=fields.Date.context_today(self))
+    start_date = fields.Date(string="Start date", required=True, default=_set_sd)
+#    start_date = fields.Date(string="Start date", required=True, default="_set_start_date")
     time_to_expire = fields.Date(string="Time to expire")
     priority = fields.Selection(string="Priority", selection=[('normal','Normal'),('high','High'),('very_high','Very High')])    
     step_technologies = fields.Text(string="Stack Technologies", required="True" )
-    duties = fields.Text(string="Duties", required="True")
+#    duties = fields.Text(string="Duties", required="True")
     requirement = fields.Text(string="Requirement",  required="True")
     phase_proj_prod = fields.Selection(string="Phase project/product", selection=[('new','New'),('existing','Existing'),('other','Other')])    
     team_size = fields.Integer(string="Team size")
     remote_allowed = fields.Selection(string="Remote Allowed?", selection=[('allowed','Allowed'),('disallowed','Disallowed'),('partially_allowed','Partiallly Allowed')])    
     project_description = fields.Text(string="Project Description")    
-    #application_ids = fields.Many2many(string="Applications", comodel_name="hr.applicant", relation="hr_job_applicant")
+    #application_ids = fields.Many2many(string="Applications", comodel_name="hr.applicant", relation="hr_job_applicant")    
+    
+    
