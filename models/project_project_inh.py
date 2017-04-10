45f158e6a0210618cd80dc47781247fde0454e22
yustas147 <yustas@simbioz.com.ua>
2017-04-10 12:07:54 +0300

    returned @api.multi

--- models/project_project_inh.py
+++ models/project_project_inh.py
@@ -12,8 +12,10 @@ class project_project(models.Model):
     job_ids = fields.One2many(string='Jobs', comodel_name='hr.job', inverse_name='project_id')
     job_count = fields.Integer(string='Job Count', compute='_get_job_count')    
     
-    @api.one    
+    @api.multi    
     def _get_job_count(self):
-        self.job_count = len(self.job_ids.mapped('id'))        
-#        _logger.info('job count is %s', unicode(self.job_count))
-        return self.job_count
+        _logger.info('self is  %s', unicode(self))
+        for rec in self:
+            rec.job_count = len(rec.job_ids.mapped('id'))        
+            _logger.info('running on is  %s', unicode(rec))
+        return True
