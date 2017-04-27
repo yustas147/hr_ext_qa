0bea0a6e56952def90fbb9d75f96f52d31271fe6
yustas147 <yustas@simbioz.com.ua>
2017-04-27 13:52:06 +0300

    lastname-firstname , applicant name gen, some fields added

--- models/hr_applicant_inh.py
+++ models/hr_applicant_inh.py
@@ -19,6 +19,24 @@ class hr_applicant(models.Model):
     _name = 'hr.applicant'    
     _inherit = 'hr.applicant'    
     
+    @api.depends('department_id','job_id')
+    def _get_applicant_name(self):
+        _logger.info('self is  %s', unicode(self))
+        for rec in self:
+            _logger.info('rec.department_id.name is  %s', unicode(rec.department_id.name))
+            _logger.info('rec.job_id.name is  %s', unicode(rec.job_id.name))
+            if (rec.department_id.name):
+                rec.name = rec.department_id.name + ' / '
+            else:
+                rec.name = 'Not in deparment /'
+            if (rec.job_id.name):
+                rec.name += rec.job_id.name            
+            _logger.info('running on is  %s', unicode(rec))
+        return True        
+        
+    
+    name = fields.Char('Subject / Application Name', compute = '_get_applicant_name', required=False)
+    
     candidate_level = fields.Selection(string="Candidate Level", selection=[('intern','Intern'),('junior','Junior'),('upper_junior','Upper Junior'),('pre_middle','Pre-Middle'),('middle','Middle'),('upper_middle','Upper Middle'),('senior','Senior')], required=True)
 #    candidate_level = fields.Selection(string="Candidate Level", selection=[('normal','Normal'),('high','High'),('very_high','Very High')])
     candidate_type = fields.Many2one(string="Candidate Type", comodel_name="hr.candidate.type")
--- models/hr_employee_inh.py
+++ models/hr_employee_inh.py
@@ -13,3 +13,16 @@ class hr_employee(models.Model):
     city = fields.Char(string="City", related='address_home_id.parent_id.city')
     corporate_email = fields.Char(string="Corporate Email", related='user_id.login')
    # propiska = fields.Char
+    last_name_ru = fields.Char(string="Last Name Ru")
+    first_name_ru = fields.Char(string="First Name Ru")
+    middle_name_ru = fields.Char(string="Middle Name Ru")
+    nickname = fields.Char(string="Nickname")
+    icq = fields.Char(string="Icq")
+    
+#we take these values from hr_applicant when create an employee or so
+    degree = fields.Char(string="Degree")
+    candidate_level = fields.Selection(string="Candidate Level", selection=[('intern','Intern'),('junior','Junior'),('upper_junior','Upper Junior'),('pre_middle','Pre-Middle'),('middle','Middle'),('upper_middle','Upper Middle'),('senior','Senior')], required=False)    
+    part_time = fields.Boolean(string="Part Time")
+    is_relocatable = fields.Boolean(string="Is Relocatable")        
+    remote_work = fields.Boolean(string="Remote Work")
+    #hr_employee_skill_data_ids
