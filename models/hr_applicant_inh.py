820c2430068793923efaa58e3826d8ae49a50b38
yustas147 <yustas@simbioz.com.ua>
2017-05-03 15:23:15 +0300

    emp from app

--- models/hr_applicant_inh.py
+++ models/hr_applicant_inh.py
@@ -2,6 +2,7 @@
 
 from openerp import models, fields, api
 import logging
+from openerp.osv import osv
 
 _logger = logging.getLogger(name='YUSTAS')
 
@@ -28,12 +29,57 @@ class hr_applicant(models.Model):
             if (rec.department_id.name):
                 rec.name = rec.department_id.name + ' / '
             else:
-                rec.name = 'Not in deparment /'
+                rec.name = ''
+ #               rec.name = 'Not in deparment /'
             if (rec.job_id.name):
                 rec.name += rec.job_id.name            
             _logger.info('running on is  %s', unicode(rec))
         return True        
         
+    
+    def create_employee_from_applicant(self, cr, uid, ids, context=None):
+        """ Create an hr.employee from the hr.applicants """
+        if context is None:
+            context = {}
+        hr_employee = self.pool.get('hr.employee')
+        model_data = self.pool.get('ir.model.data')
+        act_window = self.pool.get('ir.actions.act_window')
+        emp_id = False
+        for applicant in self.browse(cr, uid, ids, context=context):
+            address_id = contact_name = False
+            if applicant.partner_id:
+                address_id = self.pool.get('res.partner').address_get(cr, uid, [applicant.partner_id.id], ['contact'])['contact']
+                contact_name = self.pool.get('res.partner').name_get(cr, uid, [applicant.partner_id.id])[0][1]
+            if applicant.job_id and (applicant.partner_name or contact_name):
+                applicant.job_id.write({'no_of_hired_employee': applicant.job_id.no_of_hired_employee + 1})
+                create_ctx = dict(context, mail_broadcast=True)
+                emp_id = hr_employee.create(cr, uid, {'name': applicant.partner_name or contact_name,
+                                                     'job_id': applicant.job_id.id,
+                                                     'address_home_id': address_id,
+                                                     'department_id': applicant.department_id.id or False,
+                                                     'address_id': applicant.company_id and applicant.company_id.partner_id and applicant.company_id.partner_id.id or False,
+                                                     'work_email': applicant.department_id and applicant.department_id.company_id and applicant.department_id.company_id.email or False,
+                                                     'work_phone': applicant.department_id and applicant.department_id.company_id and applicant.department_id.company_id.phone or False,
+                                                     'candidate_level': applicant.candidate_level,
+                                                     'is_relocatable': applicant.is_relocatable,
+                                                     'part_time': applicant.part_time,
+                                                     'remote_work': applicant.remote_work,
+                                                     
+                                                     }, context=create_ctx)
+                self.write(cr, uid, [applicant.id], {'emp_id': emp_id}, context=context)
+                self.pool['hr.job'].message_post(
+                    cr, uid, [applicant.job_id.id],
+                    body=_('New Employee %s Hired') % applicant.partner_name if applicant.partner_name else applicant.name,
+                    subtype="hr_recruitment.mt_job_applicant_hired", context=context)
+            else:
+                raise osv.except_osv(_('Warning!'), _('You must define an Applied Job and a Contact Name for this applicant.'))
+
+        action_model, action_id = model_data.get_object_reference(cr, uid, 'hr', 'open_view_employee_list')
+        dict_act_window = act_window.read(cr, uid, [action_id], [])[0]
+        if emp_id:
+            dict_act_window['res_id'] = emp_id
+        dict_act_window['view_mode'] = 'form,tree'
+        return dict_act_window    
     
     name = fields.Char('Subject / Application Name', compute = '_get_applicant_name', required=False)
     
@@ -53,5 +99,6 @@ class hr_applicant(models.Model):
     remote_work = fields.Boolean(string="Remote Work")
     part_time = fields.Boolean(string="Part Time")
     is_relocatable = fields.Boolean(string="Is Relocatable")
+    skill_data_ids = fields.One2many(string='Skill Data', comodel_name='hr.employee.skill.data', related='emp_id.hr_employee_skill_data_ids')
     #sale_manager = fields.Many2one(comodel_name='res.users', string='Sale Manager')
     
